b435ee0ffa6bc3b39536a013d18753b7a6370a5c
agchavez <angel.chavez.325.ac@gmail.com>
2023-07-06 10:16:30 -0600

    fix: :bug: Desactivar tienda.prisma

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,7 +1,5 @@
 import { PrismaClient } from '../prisma/generated/client';
-import { PrismaClient as StoreClient } from '../prisma/generated/tienda';
 
 export const prisma = new PrismaClient();
-export const store = new StoreClient();
 
 export default prisma;
